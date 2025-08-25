96a858b5e62e362dfdecefc10c817a70c05cd3d6
alaaSnayel <alaanayel111@gmail.com>
2025-08-25 10:31:15 +0300

    instal recharts and data-fns

---
+++ src/components/hourly-temperature.tsx
@@ -0,0 +1,11 @@
+import type { ForecastData } from "@/api/types";
+
+interface HourlyTemperatureProps {
+  data: ForecastData;
+}
+
+const HourlyTemperature = ({ data }: HourlyTemperatureProps) => {
+  return <div>HourlyTemperature</div>;
+};
+
+export default HourlyTemperature;
--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,4 +1,5 @@
 import CurrentWeather from "@/components/current-weather";
+import HourlyTemperature from "@/components/hourly-temperature";
 import WeatherSkeleton from "@/components/loading-skeleton";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
@@ -111,12 +112,12 @@ const WeatherDashboard = () => {
 
       <div className="grid gap-6">
         <div>
-          {/* current weather */}
           <CurrentWeather
             data={weatherQuery.data}
             locationName={locationName}
           />
           {/* hourly temperature */}
+          <HourlyTemperature data={forecastQuery.data} />
         </div>
         <div>
           {/* details */}
