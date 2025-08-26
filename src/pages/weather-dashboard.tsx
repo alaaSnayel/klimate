33ba0bbe5e5ce4b4d65c7796af267bfa65e407e2
alaaSnayel <alaanayel111@gmail.com>
2025-08-26 11:15:46 +0300

    add Weather details

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -3,6 +3,7 @@ import HourlyTemperature from "@/components/hourly-temperature";
 import WeatherSkeleton from "@/components/loading-skeleton";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
+import WeatherDetails from "@/components/weather-details";
 import { useGeolocation } from "@/hooks/use-geolocation";
 import {
   useForecastQuery,
@@ -116,11 +117,11 @@ const WeatherDashboard = () => {
             data={weatherQuery.data}
             locationName={locationName}
           />
-          {/* hourly temperature */}
+
           <HourlyTemperature data={forecastQuery.data} />
         </div>
         <div>
-          {/* details */}
+          <WeatherDetails data={weatherQuery.data} />
           {/* forecast */}
         </div>
       </div>
